86182fb8df49dec7cd5cfb1fb69fa1bf2976a65c
Lockou <kaiojeffoficial@gmail.com>
2026-03-11 18:54:13 -0300

    fix: OAuth

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,10 @@ export default defineConfig({
       input: {
         main: fileURLToPath(new URL("./index.html", import.meta.url)),
         admin: fileURLToPath(new URL("./admin.html", import.meta.url)),
-        adminLogin: fileURLToPath(new URL("./admin-login.html", import.meta.url))
+        adminLogin: fileURLToPath(new URL("./admin-login.html", import.meta.url)),
+        robloxOauth: fileURLToPath(new URL("./roblox-oauth.html", import.meta.url)),
+        privacyPolicy: fileURLToPath(new URL("./privacy-policy.html", import.meta.url)),
+        termsOfService: fileURLToPath(new URL("./terms-of-service.html", import.meta.url))
       }
     }
   }
